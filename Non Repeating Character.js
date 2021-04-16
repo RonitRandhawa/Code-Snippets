c8996c6578b37d99f1770ab3f5acc4ee108cf9a2
Ronit Randhawa <71477770+RonitRandhawa@users.noreply.github.com>
2021-04-16 10:24:09 -0700

    Update Non Repeating Character.js

--- Non Repeating Character.js
+++ Non Repeating Character.js
@@ -1,11 +1,11 @@
 function NonrepeatingCharacter(str) {
-for (let i = 0; i < str.length; i++) {
-    		let character = str[i];
-    		if (str.indexOf(character) == i && str.indexOf(character, i + 1) == -1) {
-      		return character;
-    		}
+    for (let i = 0; i < str.length; i++) {
+    	let character = str[i];
+    	if (str.indexOf(character) == i && str.indexOf(character, i + 1) == -1) {
+      	    return character;
+    	}
   	}
-  return "No Repeating Character Found!";
+    return "No Repeating Character Found!";
 }
 
 console.log(NonrepeatingCharacter("ENTER HERE"));
