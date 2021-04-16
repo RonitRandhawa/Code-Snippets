9011111134ae4b4e561f03deb0e4e03c678ab4d8
Ronit Randhawa <71477770+RonitRandhawa@users.noreply.github.com>
2021-04-16 10:23:39 -0700

    Update Non Repeating Character.js

--- Non Repeating Character.js
+++ Non Repeating Character.js
@@ -9,3 +9,7 @@ for (let i = 0; i < str.length; i++) {
 }
 
 console.log(NonrepeatingCharacter("ENTER HERE"));
+
+/* Have the function NonrepeatingCharacter(str) take the str parameter being passed, which will contain only alphabetic characters and spaces, and return the first non-repeating character.
+For example: if str is "agettkgaeee" then your program should return k.
+The string will always contain at least one character and there will always be at least one non-repeating character. */
